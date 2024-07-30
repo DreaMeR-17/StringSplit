87e517fd71bdbfd4d1ff904bd0b723895d366fc1
DreaMeR-17 <pronka_1993@mail.ru>
2024-07-30 10:54:54 +0600

    Добавьте файлы проекта.

---
+++ StringSplit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringSplit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "I just wanna feel alive.";
+
+            string[] subText = text.Split(' ');
+
+            foreach (var  sub in subText)
+            {
+                Console.Write($"\n\nSubstring: {sub}");
+            }
+        }
+    }
+}
